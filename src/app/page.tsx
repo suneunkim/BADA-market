e2b39b24ee6d87dc4eb0c193198c149351b6b8b0
suneunkim <tjsdms89@gmail.com>
2024-01-24 19:31:18 +0900

    tailwind Prettier 플러그인 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h2>start</h2>
+      <h2 className="flex flex-col bg-red-300 p-20 text-2xl font-bold">
+        <p>start</p>
+      </h2>
+      <button className="bg-sky-300 p-20">이것은 버튼</button>
     </main>
   );
 }
