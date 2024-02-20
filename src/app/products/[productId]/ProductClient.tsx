81a433ce0c9ccebbb32357619f2faec926f736ec
suneunkim <tjsdms89@gmail.com>
2024-02-20 19:15:00 +0900

    feature: 홈 화면에 등록된 상품 보이게 변경하고 NoImage 컴포넌트 사용

--- src/app/products/[productId]/ProductClient.tsx
+++ src/app/products/[productId]/ProductClient.tsx
@@ -4,26 +4,32 @@ import ProductHead from "@/components/Product/ProductHead";
 import ProductInfo from "@/components/Product/ProductInfo";
 import Button from "@/components/UI/Button";
 import { Product, User } from "@prisma/client";
+import axios from "axios";
 import dynamic from "next/dynamic";
 import React from "react";
+import useSWR from "swr";
 
-interface Image {
-  url: string;
+export interface ImageProps {
   id: number;
+  url: string;
+  productId: number;
 }
 
 interface ProductClientProps {
-  product: Product & { user: User } & { images: Image[] };
+  product: Product & { user: User } & { images: ImageProps[] };
 }
 
 const ProductClient = ({ product }: ProductClientProps) => {
+  const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+  const { data, error } = useSWR("/api/user", fetcher);
+
   const KakaoMap = dynamic(() => import("../../../components/KakaoMap"), {
     ssr: false,
     loading: () => <div>지도 로딩중...</div>,
   });
   return (
     <Container>
-      <div className="mx-auto flex w-[350px] flex-col gap-5 sm:w-[650px]">
+      <div className="mx-auto flex w-[420px] flex-col gap-5 sm:w-[650px]">
         <ProductHead
           imageSrc={product.images.map((img) => img.url)}
           id={product.id}
@@ -40,7 +46,7 @@ const ProductClient = ({ product }: ProductClientProps) => {
           latitude={product.latitude}
           longitude={product.longitude}
         />
-        <Button label="대화하기" />
+        <Button label="대화하기" disabled={data?.ok} />
       </div>
     </Container>
   );
